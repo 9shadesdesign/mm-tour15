cdfa905ff77538297bf5b515fef17d961bef28b6
9shadesdesign <brianforcum@gmail.com>
2015-02-14 19:58:47 -0800

    add data and php files

---
+++ data/MMMarch.js
@@ -0,0 +1,42 @@
+{
+	"title": "Tour Dates",
+	
+	"concerts":[
+			  {
+				"Concert":1,
+				"EventDate":"March 07",
+                "Venue":"Gasparilla Music Festival",
+				"Location":"Tampa, FL",
+                "Tickets": "http://gasparillamusic.com/"
+			  },
+             {
+				"Concert":2,
+				"EventDate":"March 09",
+                "Venue":"Georgia Theatre",
+				"Location":"Athens, GA",
+                "Tickets": "http://www.ticketfly.com/purchase/event/785379?utm_medium=459899"
+			  },
+             {
+				"Concert":3,
+				"EventDate":"March 10",
+                "Venue":"Track 29",
+				"Location":"Chattanooga, TN",
+                "Tickets": "http://www.bandsintown.com/event/9336573/buy_tickets?app_id=fbjs_modestmouse.com&artist=Modest+Mouse&affil_code=fbjs_modestmouse.com&came_from=166"
+			  },
+         {
+				"Concert":4,
+				"EventDate":"March 12",
+                "Venue":"North Charleston PAC",
+				"Location":"Charleston, SC",
+                "Tickets": "http://www.ticketmaster.com/event/0E004E49948E47AD?REFERRAL_ID=tmfeedbuyat97249&wt.mc_id=aff_BUYAT_97249&camefrom=CFC_BUYAT_97249"
+			  },
+         {
+				"Concert":5,
+				"EventDate":"March 13",
+                "Venue":"The Ritz",
+				"Location":"Raleigh, NC",
+                "Tickets": "http://www.livenation.com/events/440703-mar-13-2015-modest-mouse"
+			  },
+             
+			]
+}
